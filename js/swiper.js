5479249ac540aa5c0a1c31a9f7bcd7bf73957469
Elborn666 <elborn18@gmail.com>
2023-09-30 11:32:19 +0300

    слайдер на доопрацювання

--- js/swiper.js
+++ js/swiper.js
@@ -1,26 +1,19 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-forward',
-      prevEl: '.swiper-button-back',
-    },
+  // autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: false,
+  // },
 
-    // // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  // // If we need pagination
+  pagination: {
+    el: '.hero__pagination',
+    clickable: true,
+  },
 
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  spaceBetween: 90,
+
+});
